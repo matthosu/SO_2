0f4673086a2d2abdd579cce5b9e2fdb4c7ea25ad
matthosu <matthosu@wp.pl>
2014-05-14 02:32:18 +0200

    dodane kolejki, został sheluder

---
+++ src/SSTF.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mateusz
+ */
+public class SSTF {
+    public class FCFS extends Kolejka {
+    public FCFS(){
+        super();
+    }
+    @Override
+    public void add(Proces proc){
+        kolejka.add(proc);
+    }
+}
+
+    
+}
