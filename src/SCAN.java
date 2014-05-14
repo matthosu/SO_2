d74a510d321c1c2583667f99531f8daee9bb9463
Mateusz Osuch <matthosu@wp.pl>
2014-05-14 19:12:16 +0200

    Tym razem powinno wyjść dobrze

--- src/SCAN.java
+++ src/SCAN.java
@@ -7,6 +7,36 @@
  *
  * @author Mateusz
  */
-public class SCAN {
-    
+public class SCAN extends Kolejka {
+    private boolean czyRosn;
+    public SCAN(){
+        super();
+        czyRosn = true;
+    }
+    public Proces get(int pozycjaGlowicy){
+        if(!kolejka.isEmpty()){
+            Proces temp;
+            int i = 0;
+            if(czyRosn){
+                while(i < kolejka.size()&&pozycjaGlowicy > kolejka.get(i++).getLoc());
+                temp = kolejka.get(i);
+            }else{
+                i = kolejka.size();
+                while(i >0 && pozycjaGlowicy < kolejka.get(i--).getLoc());
+                temp = kolejka.get(i);
+            }
+            return temp;
+        }else{
+            return null;
+        }
+    }
+    @Override
+    public void add(Proces proc){
+        int i = 0;
+        while(i < kolejka.size()&&proc.getLoc() > kolejka.get(i++).getLoc());
+        kolejka.add(i, proc);
+    }
+    public Proces get(){
+        throw new UnsupportedOperationException("IdiotProgramistException, uzyles nie tego geta");
+    }
 }
