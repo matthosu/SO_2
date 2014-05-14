ef2ad56602b584af2c9bb12efd75578c99d04d91
Mateusz Osuch <matthosu@wp.pl>
2014-05-14 19:45:50 +0200

    Tym razem powinno wyjść dobrze

--- src/CSCAN.java
+++ src/CSCAN.java
@@ -7,6 +7,33 @@
  *
  * @author Mateusz
  */
-public class CSCAN {
-    
+public class CSCAN extends Kolejka {
+    private Dysk odebrany;
+    public CSCAN(){
+        super();
+    }
+    public Proces get(int pozycjaGlowicy){
+        if(!kolejka.isEmpty()){
+            Proces temp;
+            int i = 0;
+            while(i < kolejka.size()&&pozycjaGlowicy > kolejka.get(i++).getLoc());
+            if(i == kolejka.size()){
+                increaseTotal(odebrany.przesunNaMax());
+                i = 0;
+            }
+            temp = kolejka.get(i);
+            return temp;
+        }else{
+            return null;
+        }
+    }
+    @Override
+    public void add(Proces proc){
+        int i = 0;
+        while(i < kolejka.size()&&proc.getLoc() > kolejka.get(i++).getLoc());
+        kolejka.add(i, proc);
+    }
+    public Proces get(){
+        throw new UnsupportedOperationException("IdiotProgramistException, uzyles nie tego geta");
+    }
 }
